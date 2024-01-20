5899f67f03a5d17d7d0d0e290bb96a43d1f63eda
Dahmani Mohammed Karim <109095748+Karim-Dahmani@users.noreply.github.com>
2024-01-20 12:02:58 +0100

    v2

--- src/main/java/org/sid/springcloudstreamskafka/service/PageEventService.java
+++ src/main/java/org/sid/springcloudstreamskafka/service/PageEventService.java
@@ -1,9 +1,16 @@
 package org.sid.springcloudstreamskafka.service;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.sid.springcloudstreamskafka.entities.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -21,7 +28,7 @@ public class PageEventService {
             System.out.println("************************");
         };
     }
-   // @Bean
+   @Bean
     public Supplier<PageEvent> pageEventSupplier(){
         return ()-> new PageEvent(
                     Math.random()>0.5?"P1":"P2",
@@ -30,7 +37,7 @@ public class PageEventService {
                     new Random().nextInt(1000)
                     );
     }
-    //@Bean
+    @Bean
     public Function<PageEvent,PageEvent> pageEventFunction(){
         return (input)->{
             input.setName("Page Event");
@@ -39,4 +46,19 @@ public class PageEventService {
         };
     }
 
+//    @Bean
+//    public Function<KStream<String,PageEvent>,KStream<String,Long>> StreamFunction(){
+//        return (input)->{
+//            return input
+//                    .filter((k,v)->v.getDuration()>100)
+//                    .map((k,v)->new KeyValue<>(v.getName(),0L))
+//                    .groupBy((k,v)->k,Grouped.with(Serdes.String(),Serdes.Long())) //va retourner un KTable
+//                    .windowedBy(TimeWindows.of(Duration.ofSeconds(5))) //va retourner un window a la place de String pour l'attribut Key
+//                    // cette methode-> a chaque 5s va renitialiser le nombre de visite
+//                    .count(Materialized.as("page-count"))
+//                    .toStream()  //pour converty Ktable de GroupBy to KStream
+//                    .map((k,v)->new
+//                            KeyValue<>("=>"+k.window().startTime()+" - " +k.window().endTime()+"  "+k.key(), v));
+//        };
+//    }
 }
